413b38490d571f4030b18fd1ee28ce5eb6005f3e
Lyly <khlbolatovna@gmail.com>
2022-09-22 01:07:35 +0600

    change: servername, add: func insertuser

--- user/internal/server/server.go
+++ user/internal/server/server.go
@@ -1,13 +1,23 @@
 package server
 
-type Server struct {
+import (
+	"context"
+
+	pb "github.com/4klb/pet-microservices/user/proto/user"
+)
+
+type SignInServer struct {
+	pb.UnimplementedSignInServiceServer
 }
 
-func NewServer() {
-
+func (s *SignInServer) InsertUser(ctx context.Context, user *pb.User) (*pb.StatusResp, error) {
+	//TODO
+	return &pb.StatusResp{
+		Code:        0,
+		Description: "User was added",
+	}, nil
 }
 
-func (s *Server) Run() error {
-
-	return nil
-}
+// func (s *Server) Run() error {
+// 	return nil
+// }
